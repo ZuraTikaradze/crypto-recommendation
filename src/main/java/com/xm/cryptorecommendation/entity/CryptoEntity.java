6363c2a9aba52cdb4fe35e066918d6c2a33c440c
zuratikaradze <z.tikaradze@gmail.com>
2023-02-11 04:58:36 +0400

    describe entity variables

--- src/main/java/com/xm/cryptorecommendation/entity/CryptoEntity.java
+++ src/main/java/com/xm/cryptorecommendation/entity/CryptoEntity.java
@@ -16,13 +16,29 @@ import java.util.Set;
 @Validated
 @Builder
 public class CryptoEntity extends BaseEntity {
+
+    /**
+     * Unique id
+     */
     @Id
     @SequenceGenerator(name = "crypto_id_gen", sequenceName = "crypto_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "crypto_id_gen")
     private Long id;
+
+    /**
+     * Crypto name
+     */
     private String name;
+
+    /**
+     * Crypto active status
+     */
     private boolean active;
 
+
+    /**
+     * Crypto prices
+     */
     @OneToMany(mappedBy = "crypto", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<PriceEntity> prices = new HashSet<>();
 }
--- src/main/java/com/xm/cryptorecommendation/entity/PriceEntity.java
+++ src/main/java/com/xm/cryptorecommendation/entity/PriceEntity.java
@@ -18,16 +18,32 @@ import java.time.Instant;
 @AllArgsConstructor
 @Builder
 public class PriceEntity extends BaseEntity {
+
+    /**
+     * Unique id
+     */
     @Id
     @SequenceGenerator(name = "price_id_gen", sequenceName = "price_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "price_id_gen")
     private Long id;
+
+    /**
+     * Price
+     */
     @NotNull
     private BigDecimal price;
+
+    /**
+     * Price timestamp
+     */
     @NotNull
     private Instant priceTimestamp;
     @Valid
     @ManyToOne(fetch = FetchType.LAZY)
+
+    /**
+     * Crypto
+     */
     @Cascade(CascadeType.ALL)
     private CryptoEntity crypto;
 }
